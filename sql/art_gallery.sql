381cbc1a2179bdc78f95d5ae5f0bfb61f3cf7a8c
TrannqGCC210042 <tranqgcc210042@fpt.edu.vn>
2023-05-15 16:30:21 +0700

    delete on database

--- sql/art_gallery.sql
+++ sql/art_gallery.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 13, 2023 at 10:14 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.1.17
+-- Generation Time: May 15, 2023 at 11:29 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.0.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,17 +31,10 @@ CREATE TABLE `art` (
   `a_id` int(11) NOT NULL,
   `a_name` varchar(255) NOT NULL,
   `a_date` date NOT NULL,
-  `a_category` varchar(255) NOT NULL,
+  `a_category` varchar(6) NOT NULL,
   `a_description` varchar(2000) NOT NULL,
   `a_image` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `art`
---
-
-INSERT INTO `art` (`a_id`, `a_name`, `a_date`, `a_category`, `a_description`, `a_image`) VALUES
-(0, '12457', '2023-06-01', 'C002', 'sdfhjklkjytrd', 'pillows-white-background.jpg');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -52,15 +45,7 @@ INSERT INTO `art` (`a_id`, `a_name`, `a_date`, `a_category`, `a_description`, `a
 CREATE TABLE `category` (
   `cat_id` varchar(6) NOT NULL,
   `cat_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `category`
---
-
-INSERT INTO `category` (`cat_id`, `cat_name`) VALUES
-('C001', 'Poster'),
-('C002', 'Banner');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
@@ -70,13 +55,24 @@ INSERT INTO `category` (`cat_id`, `cat_name`) VALUES
 -- Indexes for table `art`
 --
 ALTER TABLE `art`
-  ADD PRIMARY KEY (`a_id`);
+  ADD PRIMARY KEY (`a_id`),
+  ADD KEY `a_category` (`a_category`);
 
 --
 -- Indexes for table `category`
 --
 ALTER TABLE `category`
   ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `art`
+--
+ALTER TABLE `art`
+  ADD CONSTRAINT `art_ibfk_1` FOREIGN KEY (`a_category`) REFERENCES `category` (`cat_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
